b2d42f441f24755892596086b2c7d983d6d2be11
Muhammad Umar <one@pop-os.localdomain>
2023-11-07 07:50:46 +0700

    first commit

---
+++ app/build.gradle.kts
@@ -0,0 +1,68 @@
+plugins {
+    id("com.android.application")
+    id("org.jetbrains.kotlin.android")
+    id("com.google.devtools.ksp").version("1.6.10-1.0.4")
+}
+
+android {
+    namespace = "com.example.newsapp"
+    compileSdk = 34
+
+    defaultConfig {
+        applicationId = "com.example.newsapp"
+        minSdk = 28
+        targetSdk = 33
+        versionCode = 1
+        versionName = "1.0"
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+    kotlinOptions {
+        jvmTarget = "1.8"
+    }
+}
+
+dependencies {
+
+    implementation("androidx.core:core-ktx:1.9.0")
+    implementation("androidx.appcompat:appcompat:1.6.1")
+    implementation("com.google.android.material:material:1.10.0")
+    implementation("androidx.constraintlayout:constraintlayout:2.1.4")
+    testImplementation("junit:junit:4.13.2")
+    androidTestImplementation("androidx.test.ext:junit:1.1.5")
+    androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+
+    //SplashScreen Aplikasi tampilan awal sebelum aplikasi dibuka
+    implementation("androidx.core:core-splashscreen:1.1.0-alpha02")
+
+    //Moshi parsing Kotlin Codegan
+    ksp("com.squareup.moshi:moshi-kotlin-codegen:1.14.0")
+
+    //Depend via Maven
+    implementation("com.squareup.moshi:moshi:1.14.0")
+
+    //Impementation penggunaan donwload gambar menggunkan fungsi picasso
+    implementation ("com.squareup.picasso:picasso:2.8")
+
+    //Retrofit
+    implementation ("com.squareup.retrofit2:retrofit:2.9.0")
+    implementation ("com.squareup.retrofit2:converter-moshi:2.9.0")
+
+
+
+
+}
